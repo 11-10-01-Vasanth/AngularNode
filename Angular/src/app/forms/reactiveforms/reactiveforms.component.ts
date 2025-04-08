63025d495803bdf60c6d52e29341aae430df62f3
11-10-01-Vasanth <mvasanth11102001@gmail.com>
2025-04-08 18:39:33 +0530

    Reactive forms

--- Angular/src/app/forms/reactiveforms/reactiveforms.component.ts
+++ Angular/src/app/forms/reactiveforms/reactiveforms.component.ts
@@ -8,9 +8,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './reactiveforms.component.css'
 })
 export class ReactiveformsComponent {
-public handleSubmit() {
-  console.log(this.personForm.value);
+
+// public handleSubmit() {
+//   console.log(this.personForm.value);
+// }
+
+public handleSubmit(person: any) {
+  console.log(person);
 }
+
+  firstName: any="Vasantha kumar";
+  lastName:any="M";
+  email:any="vasanth@gmail.com";
+  mobile:any=9626542367;
+  gender:any="M";
+  street:any="abc"
+  city:any="vnr"
+  state:any="Tamilnadu"
 
   states = [
     "Tamilnadu","Kerela","Kolkata"
@@ -21,9 +35,9 @@ public handleSubmit() {
   ngOnInit(){
     this.personForm = new FormGroup({
       firstName: new FormControl("Vasantha kumar",[Validators.required,Validators.minLength(3),Validators.maxLength(15)]),
-      lastName: new FormControl("M",[Validators.required,Validators.minLength(3),Validators.maxLength(15)]),
+      lastName: new FormControl("M",[Validators.required,Validators.maxLength(5)]),
       email: new FormControl("vasanth@gmail.com",[Validators.required]),
-      gender: new FormControl("M",[Validators.required]),
+      gender: new FormControl("M"),
       address: new FormGroup({
         street: new FormControl("abc"),
         city: new FormControl("vnr"),
